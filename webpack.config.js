5e1b41905b7ce14a401cb4eb08e201e8f6ef555a
Jack Hanford <jackhanford@gmail.com>
2016-07-03 13:02:45 -0700

    add additional css features and webpack loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
-var webpack = require('webpack')
-var autoprefixer = require('autoprefixer')
+const webpack = require('webpack')
+const autoprefixer = require('autoprefixer')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const nested = require('postcss-nested')
+const cssVariables = require('postcss-css-variables')
 
 module.exports = {
   entry: './src/main.js',
@@ -17,13 +20,22 @@ module.exports = {
         presets: ['react', 'es2015']
       }
     }, {
-      test: /\.css/,
+      test: /\.css$/,
       loader: 'style-loader!css-loader?modules!postcss-loader'
+    }, {
+      test: /\.(jpg|svg|woff)$/,
+      loader: 'file-loader'
+    }, {
+      test: /\.(html)$/,
+      loader: 'html?attrs=false'
+    }, {
+      test: /\.(json)$/,
+      loader: 'json-loader'
     }]
   },
   postcss: function () {
     return {
-      defaults: [autoprefixer],
+      defaults: [autoprefixer, nested, cssVariables],
       cleaner: [ autoprefixer({ browsers: ['last 3 browsers'] }) ]
     }
   },
